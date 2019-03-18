efec9273830b7e41c0378c29e91b2c81d14ad151
Ivan <Ivan@LAPTOP-O8C7K94C>
2019-03-18 16:32:39 +0100

    Añadida clase CustomerRowMapper

---
+++ proyectoNuevo/src/main/java/es/uji/proyecto/dao/CustomerRowMapper.java
@@ -0,0 +1,26 @@
+package es.uji.proyecto.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import es.uji.proyecto.model.Customer;
+
+public class CustomerRowMapper implements RowMapper<Customer>{
+
+	
+	public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+		// TODO Auto-generated method stub
+			Customer customer = new Customer();
+			customer.setNid(rs.getString("nid"));
+			customer.setName(rs.getString("name"));
+			customer.setGender(rs.getString("gender"));
+			customer.setEmail(rs.getString("email"));
+			return customer;
+	}
+	
+	
+	
+}
+
