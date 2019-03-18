a5b0e2c1cc584ef36d4005b20b67eed625c933ba
Ivan <Ivan@LAPTOP-O8C7K94C>
2019-03-18 19:36:31 +0100

    Añadido el controlador de Customer y sus clases html para gestionar contenido

---
+++ proyectoNuevo/src/main/java/es/uji/proyecto/controller/CustomerController.java
@@ -0,0 +1,43 @@
+package es.uji.proyecto.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import es.uji.proyecto.dao.CustomerDao;
+import es.uji.proyecto.model.Customer;
+
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@Autowired
+	private CustomerDao customerDao;
+		
+	
+	@RequestMapping("/list")
+	public String listProves(Model model) {
+		model.addAttribute("proves", customerDao.getCustomers());
+		return "customer/list";
+	}
+	
+	@RequestMapping(value="/add") 
+	public String addCustomer(Model model) {
+		model.addAttribute("customer", new Customer());
+		return "customer/add";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST) 
+	public String processAddSubmit(@ModelAttribute("customer") Customer customer,
+			BindingResult bindingResult) {
+		 if (bindingResult.hasErrors())
+				return "customer/add";
+		 customerDao.addCustomer(customer);
+		 return "redirect:list";
+	 }
+}
--- proyectoNuevo/src/main/java/es/uji/proyecto/dao/CustomerDao.java
+++ proyectoNuevo/src/main/java/es/uji/proyecto/dao/CustomerDao.java
@@ -22,24 +22,24 @@ public class CustomerDao {
 	   }
 
 	   /* Afegeix el usuari a la base de dades */
-	   void addCustomer(Customer customer) {
+	   public void addCustomer(Customer customer) {
 	       jdbcTemplate.update("INSERT INTO Customer VALUES(?, ?, ?, ?)",
 		       		customer.getNid(), customer.getName(), customer.getGender(), customer.getEmail());
 	   }
 
 	   /* Esborra el usuari de la base de dades */
-	   void deleteCustomer(Customer customer) {
+	   public void deleteCustomer(Customer customer) {
 	       jdbcTemplate.update("DELETE FROM Customer WHERE nom=?", customer.getNid());
 	   }
 
 	   /* Actualitza els atributs del usuari
 	      (excepte el nid, que és la clau primària) */
-	   void updateCustomer(Customer customer) {
+	   public void updateCustomer(Customer customer) {
 	       jdbcTemplate.update("UPDATE Customer SET name=?,gender=?, email=? WHERE nid=?",customer.getName(),customer.getGender(),customer.getEmail(), customer.getNid());
 	   }
 
 	   /* Obté el usuari amb el nom donat. Torna null si no existeix. */
-	   Customer getCustomer(Customer customerName) {
+	   public Customer getCustomer(Customer customerName) {
 	       try {
 	           return jdbcTemplate.queryForObject("SELECT * FROM Customer WHERE nid = ?",
 		      		     new CustomerRowMapper(),
@@ -51,7 +51,7 @@ public class CustomerDao {
 	   }
 
 	   /* Obté tots els usuaris. Torna una llista buida si no n'hi ha cap. */
-	   List<Customer> getCustomers() {
+	   public List<Customer> getCustomers() {
 	       try {
 	           return jdbcTemplate.query("SELECT * FROM Customer", new CustomerRowMapper());
 	       }
